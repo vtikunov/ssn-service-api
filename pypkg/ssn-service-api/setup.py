950f49f991aa864001208c513b4dede3ce14f77a
Vladimir Tikunov <vtikunov@gmail.com>
2021-11-09 12:11:47 +0300

    [version] Fixed deployed version

--- pypkg/ssn-service-api/setup.py
+++ pypkg/ssn-service-api/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="grpc-ssn-service-api",
-    version="4.0.0",
+    version="0.4.0",
     author="vtikunov",
     author_email="vtikunov@yandex.ru",
     description="GRPC python client for ssn-service-api",
