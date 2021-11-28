b0717a5c62852c3c4c89b42fb7011883dbd101f3
Vladimir Tikunov <vtikunov@gmail.com>
2021-11-28 03:22:06 +0300

    [fix] Fixed isHasPrevPage & isHasNextPage flags in List

--- internal/facade/grpc/api/api_list_service.go
+++ internal/facade/grpc/api/api_list_service.go
@@ -19,11 +19,22 @@ func (o *serviceAPI) ListServicesV1(ctx context.Context, req *pbf.ListServicesV1
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	services, err := o.srvService.List(ctx, req.Offset, req.Limit)
+	var isHasPrevPage, isHasNextPage bool
+
+	if req.Offset > 0 {
+		isHasPrevPage = true
+	}
+
+	services, err := o.srvService.List(ctx, req.Offset, req.Limit+1)
 	if err != nil {
 		logger.ErrorKV(ctx, "ListServicesV1 - failed", "err", err)
 
 		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	if len(services) > int(req.Limit) {
+		isHasNextPage = true
+		services = services[:len(services)-1]
 	}
 
 	servicesPb := make([]*pbf.Service, 0, len(services))
@@ -33,6 +44,8 @@ func (o *serviceAPI) ListServicesV1(ctx context.Context, req *pbf.ListServicesV1
 	}
 
 	return &pbf.ListServicesV1Response{
-		Services: servicesPb,
+		Services:      servicesPb,
+		IsHasPrevPage: isHasPrevPage,
+		IsHasNextPage: isHasNextPage,
 	}, nil
 }
